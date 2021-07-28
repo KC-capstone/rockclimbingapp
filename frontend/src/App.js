219f6e3d4eeee9b0fabc11fdcc5a3638da844571
Greg P <gplechaty@gmail.com>
2021-07-28 11:56:36 -0600

    profile page html/css  renders correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
-
 import Index from './components/pages/Index/Index.js';
 import Profile from './components/pages/Profile/Profile.js';
 import ClimbDetail from './components/pages/ClimbDetail/ClimbDetail.js';
 
 function App() {
+  const [page, setPage] = useState("profile");
+
+  function onProfileClick() {
+    setPage('profile');
+  }
+
   return (
-    <Router>
-    <div className="App">
-      <h3 className="App-title">Cool Animals</h3>
-
-      <Link to="/">Index</Link>
-      <Link to="/profile/">Stuff about profiles</Link>
-      <Link to="climb-detail/">Also stuff on climb details</Link>
-
-      <Switch>
-        <Route exact path='/' component={Index} />
-        <Route exact path='/profile/' component={Profile} />
-        <Route exact path='/climb-detail/' component={ClimbDetail} />
-      </Switch>
-
-      <p>Copyright 1997 - All Rights Reserved</p>
+    <div>
+    {page === 'profile' ? (
+      <Index />
+    ) : null}
     </div>
-    </Router>
   );
 }
 
--- frontend/src/components/pages/Profile/Profile.js
+++ frontend/src/components/pages/Profile/Profile.js
@@ -1,11 +1,76 @@
 import React from 'react';
+import './styles.css';
+import defaultProfile from '../../../assets/profileDefault.png';
+import profileBackground from '../../../assets/profileBackground.jpg';
+import exampleClimb from '../../../assets/exampleClimb.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+
 
 function Profile() {
   return (
-    <div>
-      <p>Let's talk about Profiles. They live in the water.</p>
-      <img src="https://i.imgur.com/bG3nBgI.jpg" alt="dolphin" />
+    <div className="pageCenter">
+    <div className="nav">
+        <a  className="nav__link" href="./profile.html"><div></div>Profile</a>
+        <a  className="nav__link" href="#"><div>Stats</div></a>
+        <a  className="nav__link" href="./climbs.html"><div>Climbs</div></a>
+        <a  className="nav__link nav__link--logActivity" href="./logActivity.html">
+            <div className="nav__link--centerContainer">
+                <div className="nav__link--center">
+                    <i className="fas fa-plus-square"></i>
+                    <FontAwesomeIcon icon={faPlusSquare} />
+                </div>
+            </div>
+        </a>
     </div>
+    
+
+    <div className="profileBlock profileBlock--center">
+        <div className="profileBlock__user">
+            <div className="profileBlock__picture">
+                <img className="profileBlock__picture--img" src={defaultProfile}/>
+            </div>
+            <h4 className="profileBlock__user--name">Greg P</h4>
+        </div>
+        
+        <div className="profileBlock__imgLarge">
+            <img className="profileBlock__imgLarge--pic" src={profileBackground}/>
+        </div>
+    </div>
+    <div className="profileBlock profileBlock--spaceAround">
+        <div className="profileBlock__statOverview">
+            <p className="profileBlock__statOverview--num">9</p>
+            <p className="profileBlock__statOverview--text">Activities in the last month</p>
+        </div>
+        <div className="profileBlock__statOverview">
+            <p className="profileBlock__statOverview--num">52</p>
+            <p className="profileBlock__statOverview--text">Routes Completed</p>
+        </div>
+        <div className="profileBlock__statOverview">
+            <p className="profileBlock__statOverview--num">V4</p>
+            <p className="profileBlock__statOverview--text">Toughest Route</p>
+        </div>
+    </div>
+
+    <div className="climbCard climbCard--shadow profileBlock--center">
+        <div className="climbCard--positionOne">
+            <div className=".climbCard--smallOne">
+                <p className="climbCard__title"><a className="cardLink" href="./climbDetail.html">Brooklyn Boulders, Partner Climb</a></p>
+            </div>
+            <div className="climbCard--smallThree">
+                <p className="climbCard__detail">Date: Feb 14, 2019</p>
+                <p className="climbCard__detail">Climbs: 6</p>
+                <p className="climbCard__detail">Toughest climb: V3</p>
+                <div className="climbCard__desc">
+                    <p className="climbCard__desc--overflow">Absolutely loved this! I'd never heard of this before, but they set up a climd that could only be completed with two people! The first pat of the climb wasn't unusual, but then you reach a point where you can't continue without one partner helping the other. We finished it by having one person grab the other's leg and lifting themselves up. Both partners have to top out to complete the climb. Hope they do this again next year!</p>
+                </div>
+            </div>
+        </div>
+        <div className="climbCard__pictureBox climbCard--smallTwo">
+            <img className="climbCard__pictureBox--pic" src={exampleClimb}/>       
+        </div>
+    </div>
+</div>
   );
 }
 
